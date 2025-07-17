e2243953496cf178750221947eeecf845d4ba58e
KyungWoon Cho <cezanne@codemayo.com>
2025-07-17 14:10:20 +0900

    timeout and interval alignment

--- enelog.c
+++ enelog.c
@@ -11,6 +11,7 @@
 #include <getopt.h>
 
 static unsigned long     interval = 1000000; // Default interval in usecs
+static unsigned timeout = 120; // Default timeout in seconds
 
 static void
 usage(void)
@@ -75,19 +76,43 @@ setup_current_time_str(char *timebuf)
 }
 
 static void
+wait_until_aligned_interval(void)
+{
+        struct timespec ts;
+        clock_gettime(CLOCK_REALTIME, &ts);
+
+        unsigned intv = interval / 1000000; // Convert microseconds to seconds
+        unsigned sec = ts.tv_sec % 60;
+        unsigned nsec = ts.tv_nsec;
+        unsigned wait_sec = (sec % intv == 0) ? 0 : intv - (sec % intv);
+        long wait_usec = (1000000000L - nsec) / 1000;
+
+        // 전체 대기 시간 (마이크로초)
+        long total_wait = wait_sec * 1000000L + wait_usec;
+        if (total_wait > 0)
+                usleep(total_wait);
+}
+
+static void
 log_energy(int fd)
 {
         char    timebuf[32];
-        struct timespec ts_last;
+        struct timespec ts_start, ts_last;
         double  energy_last;
 
-        clock_gettime(CLOCK_MONOTONIC, &ts_last);
+        wait_until_aligned_interval();
+
+        clock_gettime(CLOCK_MONOTONIC, &ts_start);
+        ts_last = ts_start;
         energy_last = read_energy(fd);
 
         while (1) {
                 struct timespec ts_cur;
 
                 clock_gettime(CLOCK_MONOTONIC, &ts_cur);
+                uint64_t        elapsed_from_start = get_usec_elapsed(&ts_start, &ts_cur);
+                if (elapsed_from_start >= timeout * 1000000)
+                        break;
                 uint64_t        elapsed = get_usec_elapsed(&ts_last, &ts_cur);
                 if (elapsed >= interval) { // Convert seconds to microseconds
                         double  energy_cur = read_energy(fd);
@@ -109,10 +134,17 @@ parse_args(int argc, char *argv[])
 {
         int     c, opt;
 
-        while ((c = getopt(argc, argv, "i:h")) != -1) {
+        while ((c = getopt(argc, argv, "i:t:h")) != -1) {
                 switch (c) {
                         case 'i':
                                 interval = strtoul(optarg, NULL, 10) * 1000000;
+                                break;
+                        case 't':
+                                timeout = strtoul(optarg, NULL, 10);
+                                if (timeout <= 0) {
+                                        fprintf(stderr, "Invalid timeout value: %s\n", optarg);
+                                        exit(EXIT_FAILURE);
+                                }
                                 break;
                         case 'h':
                                 usage();
